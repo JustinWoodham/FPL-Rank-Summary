1d8f4cf1ca6600c26d979234c61b94377fbc42e1
Justin Woodham <WoodhamJ@cardiff.ac.uk>
2026-02-05 22:21:22 +0000

    v1.0

---
+++ fplRankSummary.py
@@ -0,0 +1,55 @@
+import requests
+import json
+import pandas as pd
+from pprint import pprint
+import matplotlib.pyplot as plt
+
+url = 'https://fantasy.premierleague.com/api'
+
+
+# enter your teamID where it says 'TEAMID'
+# instructions on how to get your FPL team id can be found here: https://fpl.team/find-fpl-team-id/
+managerHistory = '/entry/6740264/history/'
+managerName = '/entry/6740264/'
+
+# getting requests for FPL data
+r = requests.get(url+managerHistory).json()
+sum = pd.json_normalize(r['current'])
+l = requests.get(url+managerName).json()
+fplName = l['name']
+
+
+#forming a dataset for the FPL data
+gameWeeks = []
+gwRank = []
+for week in sum['event']:
+    rank = sum.loc[sum['event'] == week, 'overall_rank'].item()
+    gwRank.append(rank)
+    gameWeeks.append(week)
+
+data = {'Gameweek': gameWeeks, 'Gameweek Rank': gwRank}
+df = pd.DataFrame(data=data)
+# uncomment this to view your data in table form
+print(df)
+
+
+# plotting and titling
+plt.plot(df['Gameweek'], df['Gameweek Rank'])
+plt.title(f"FPL Rank for {fplName} this season") 
+
+# tick and axis altering
+plt.ticklabel_format(style='plain') # changes the ticks from scientific notation to plain numerical
+plt.ylim(bottom=0) # sets the range of the y-axis to start from 0
+plt.gca().invert_yaxis()  #inverts the rank so 0 is the top
+
+#alters y ticks to have commas representing thousands and millions
+current_values = plt.gca().get_yticks()
+plt.gca().set_yticklabels(['{:,.0f}'.format(x) for x in current_values])
+
+#labeling
+plt.xlabel("Gameweek")
+plt.ylabel("Rank")
+
+plt.grid()
+plt.show()
+
